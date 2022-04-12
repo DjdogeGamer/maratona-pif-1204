51260bf32ebcfc2df2a7e54260ff04694b9f9eed
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-04-12 15:59:53 -0300

    Add comments to 01.c

--- 01.c
+++ 01.c
@@ -2,6 +2,14 @@
 
 int main ()
 {
+    /*
+    Essa e a questao mais simples, e era uma pegadinha.
+    Não necessariamente precisa implementar uma matriz,
+    essa seria a implementacao mais rapida e eficiente
+    dado o tempo da maratona. As proximas tem implementacoes
+    mais sofisticadas.
+    */
+    
     int pedras_brancas, pedras_pretas;
     
     scanf("%d %d", &pedras_brancas, &pedras_pretas);
