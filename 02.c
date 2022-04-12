c6d00dd6b9462aaafdcbba1cb133a19c81fc1b1f
Vitor Carvalho <vitor.velozo2003@gmail.com>
2022-04-12 16:28:16 -0300

    Refactors 02.c

--- 02.c
+++ 02.c
@@ -1,59 +1,54 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     int subnum, num;
-    do{
-        scanf("%d %d", &num, &subnum);
-        
-        //Conta o numero de digitos dos numeros inteiros
-        int sub_len=0, num_len=0;
+    do
+    {
+        printf("Informe o numero completo: ");
+        scanf("%d", &num);
+        getchar();
+
+        printf("Informe os ultimos digitos do numero: ");
+        scanf("%d", &subnum);
+        getchar();
+
+        // Conta o numero de digitos dos numeros inteiros
+        int sub_len = 0, num_len = 0;
         int aux_sub = subnum, aux_num = num;
-        while(aux_sub != 0)  
-        {  
-            aux_sub = aux_sub/10;  
-            sub_len++;  
-        }
-        while(aux_num != 0)
+        while (aux_sub != 0)
         {
-            aux_num = aux_num/10;
+            aux_sub = aux_sub / 10;
+            sub_len++;
+        }
+        while (aux_num != 0)
+        {
+            aux_num = aux_num / 10;
             num_len++;
         }
-        
-        //Primeiramente verifica se o número que deveria ser subnum é maior
+
+        // Primeiramente verifica se o número que deveria ser subnum é maior
         if (sub_len > num_len)
         {
-            printf("%d => nao encaixa", subnum);
+            printf("%d => nao encaixa\n", subnum);
         }
-        //Caso seja igual ou menor, verifica se os numeros sao iguais
+        // Caso seja igual ou menor, verifica se os numeros sao iguais
         else
         {
-            int count = 1, divider = 10, verifier = 0;
-            while(sub_len!=0)
+            int verifier = 0;
+            int divider = round(pow(10, sub_len));
+
+            if (subnum % divider == num % divider)
             {
-                for (int i = 0; i < count-1; i++)
-                {
-                    divider *= 10;
-                }
-                if (subnum % divider == num % divider)
-                {
-                    verifier = 1;
-                }
-                else
-                {
-                    verifier = 0;
-                    sub_len = 0;
-                    printf("%d => nao encaixa\n", subnum);
-                    break;
-                }
-                sub_len--;
-            }
-            
-            if (verifier == 1)
-            {
+                verifier = 1;
                 printf("%d => encaixa\n", subnum);
             }
+            else
+            {
+                verifier = 0;
+                printf("%d => nao encaixa\n", subnum);
+            }
         }
-    }while(1);
+    } while (subnum != 0 || num != 0);
 }
-
